5e8b232db2c12bcdb3ef5f26becc9e92308cae4a
Carla H <hetterichcarla@gmail.com>
2025-11-27 02:30:13 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
